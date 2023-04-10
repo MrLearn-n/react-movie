1f919ec538f4992e726ac25dfe22b9c01801d990
MrLearn <learnmr67@gmail.com>
2023-04-10 22:57:43 +0300

    сделал смену цвета у логотипа

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import Logo from './../../assets/header/headerLogo.svg';
+import React, { useState } from 'react'
 import {AiOutlineSearch} from 'react-icons/ai';
 import {AiOutlineShoppingCart} from 'react-icons/ai';
 import {BsFillSunFill} from 'react-icons/bs';
 import {BiBookmark} from 'react-icons//bi';
-import style from './Header.module.css';
 
-export default function Header({btnShowCartModal, btnShowFavoriteModal}) {
+import style from './Header.module.css';
+import { Logo } from './Logo';
+
+export default function Header({btnShowCartModal, btnShowFavoriteModal, cartInfo, changeTheme, theme}) {    
+
     return (
         <div className={style.flex_content}>
-            <div className=''>
-                <img src={Logo}/>
-            </div>
+            {theme === 'light' ? 
+                <Logo fill='black' /> 
+                : 
+                <Logo fill='white' />
+            }
             <div className={style.search}>
                 <input type='search' placeholder="Поиск" className={style.input} />
                 <AiOutlineSearch className={style.searchIcon} />
             </div>
             <div className={style.borderIcon}>
                 <AiOutlineShoppingCart className={style.cartIcon} onClick = {() => btnShowCartModal()}/>
+                {cartInfo.length > 0
+                    ? 
+                        <div className={style.counter}>
+                        <p>{cartInfo.length}</p>
+                        </div>    
+                    :
+                        <div className=''> </div>
+            }
             </div>
-            <div className={style.borderIcon}>
+            <div className={style.borderIcon} onClick = {changeTheme}>
                 <BsFillSunFill className={style.sunIcon}/>
             </div>
             <div className={style.favoriteBtn} onClick = {() => btnShowFavoriteModal()}>
