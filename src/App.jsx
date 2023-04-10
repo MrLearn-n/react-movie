aa3bf9455b44d2ed33cf8241e5ac3f3e2b215a21
MrLearn <learnmr67@gmail.com>
2023-04-10 22:46:51 +0300

    сменил формат файла  js => jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './pages/Home';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='wrapper'>
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
