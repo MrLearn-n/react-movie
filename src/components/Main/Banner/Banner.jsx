37e458c48677eb86bc01fc81a8fb669626544d62
MrLearn <learnmr67@gmail.com>
2023-04-10 23:00:03 +0300

    небольшие правки => свойство draggable = flase у изображений

--- src/components/Main/Banner/Banner.jsx
+++ src/components/Main/Banner/Banner.jsx
@@ -6,20 +6,19 @@ import bannerImg1 from './../../../assets/main/bannerImg1.png';
 import bannerImg2 from './../../../assets/main/bannerImg2.png';
 import bannerImg3 from './../../../assets/main/bannerImg3.png';
 
-//настроить получение изображений через axios
 
 export default function Banner() {
     return (
         <div className={style.banner_body}>
             <div className={style.banner_content}>
                 <div className={style.banner_text}>
-                    <img src={bannerLogo} />
+                    <img src={bannerLogo} draggable = {false} alt = 'logo'/>
                     <h1 className={style.txt}>Выбирайте те фильмы, которые хотите посмотреть</h1>
                 </div>
                 <div className={style.banner_img}>
-                    <img src={bannerImg1} draggable = {false}/>
-                    <img src={bannerImg2} draggable = {false}/>
-                    <img src={bannerImg3} draggable = {false}/>
+                    <img src={bannerImg1} draggable = {false} alt = 'movie-poster'/>
+                    <img src={bannerImg2} draggable = {false} alt = 'movie-poster'/>
+                    <img src={bannerImg3} draggable = {false} alt = 'movie-poster'/>
                 </div>
             </div>
         </div>
